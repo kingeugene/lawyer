9192445e102002be960cfe569ac532e090f42708
kingeugene <king.eugene1503@gmail.com>
2019-11-03 23:05:29 +0200

    first commit

---
+++ base/header.php
@@ -0,0 +1,28 @@
+<header class="header">
+    <div class="container headerWrap">
+        <div class="logo"><img src="/base/logo.png" alt="logo"></div>
+        <ul class="navWrap">
+            <li class="nav">Про мене</li>
+            <li class="nav">Послуги</li>
+            <li class="nav">Ціни</li>
+            <li class="nav">Контакти</li>
+            <li class="nav">Новини</li>
+        </ul>
+    </div>
+    <div class="menuBurger">
+        <div class="burger-container">
+            <div id="burger">
+                <div class="bar topBar"></div>
+                <div class="bar btmBar"></div>
+            </div>
+        </div>
+        <ul class="menu">
+            <li class="menu-item"><a href="">Про мене</a></li>
+            <li class="menu-item"><a href="">Послуги</a></li>
+            <li class="menu-item"><a href="">Ціни</a></li>
+            <li class="menu-item"><a href="">Контакти</a></li>
+            <li class="menu-item"><a href="">Новини</a></li>
+        </ul>
+    </div>
+
+</header>
